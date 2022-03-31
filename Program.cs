2ffcfa582de570a590776ec3dba42242ff74627c
Michael Marsico <michaelrmarsico@icloud.com>
2022-03-30 22:16:31 -0500

    Learning Part 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿foreach (string file in GetSales())
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> GetSales()
+{
+    List<string> list = new List<string>();
+
+    IEnumerable<string> files = Directory.EnumerateFiles("stores", "sales.json", SearchOption.AllDirectories);
+
+    foreach (string file in files)
+    {
+        list.Add(file);
+    }
+
+    return list;
+}
